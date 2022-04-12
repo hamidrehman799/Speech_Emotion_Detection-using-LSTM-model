9950e89fc9478f5450ea8aa5d29b1c8f8f53349d
Hamid Rehman <86568465+hamidrehman799@users.noreply.github.com>
2022-04-12 12:49:06 -0700

    Completed

---
+++ Speech Emotion Detection In Flask/app.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+### General imports ###
+from __future__ import division
+import numpy as np
+import pandas as pd
+import time
+import re
+import os
+from collections import Counter
+
+### Flask imports
+import requests
+from flask import Flask, render_template, session, request, redirect, flash, Response
+
+### Audio imports ###
+from speechEmotionRecognition import *
+
+
+
+# Flask config
+app = Flask(__name__)
+app.secret_key = b'(\xee\x00\xd4\xce"\xcf\xe8@\r\xde\xfc\xbdJ\x08W'
+app.config['UPLOAD_FOLDER'] = '/Upload'
+
+################################################################################
+################################## INDEX #######################################
+################################################################################
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+################################################################################
+############################### AUDIO Prediction ################################
+################################################################################
+
+# Audio Index
+@app.route('/audio_index', methods=['POST'])
+def audio_index():
+    return render_template('audio.html', display_button=False)
+
+# Audio Recording
+@app.route('/audio_recording', methods=("POST", "GET"))
+def audio_recording():
+
+    # Instanciate new SpeechEmotionRecognition object
+    SER = speechEmotionRecognition()
+
+    # Voice Recording
+    rec_duration = 10 # in sec
+    rec_sub_dir = os.path.join('voice_recording.wav')
+    SER.voice_recording(rec_sub_dir, duration=rec_duration)
+
+    # Send Flash message
+    flash("The recording is over! You now have the opportunity to do an analysis of your emotions. If you wish, you can also choose to record yourself again.")
+
+    return render_template('audio.html', display_button=True)
+
+
+# Audio Emotion Analysis
+@app.route('/audio_analysis', methods=("POST", "GET"))
+def audio_analysis():
+
+    # Sub dir to speech emotion recognition model
+    model_sub_dir = os.path.join('Models', 'audio.hdf5')
+
+    # Instanciate new SpeechEmotionRecognition object
+    SER = speechEmotionRecognition(model_sub_dir)
+
+    # Voice Record sub dir
+    rec_sub_dir = os.path.join('voice_recording.wav')
+
+    # Predict emotion in voice at each time step
+    step = 1 # in sec
+    sample_rate = 16000 # in kHz
+    emotions, timestamp = SER.predict_emotion_from_file(rec_sub_dir, chunk_step=step*sample_rate)
+
+    # Export predicted emotions to .txt format
+    SER.prediction_to_csv(emotions, os.path.join("static","js", "audio_emotions.txt"), mode='w')
+    SER.prediction_to_csv(emotions, os.path.join("static","js", "audio_emotions_other.txt"), mode='a')
+
+    # Get most common emotion during the interview
+    major_emotion = max(set(emotions), key=emotions.count)
+
+    # Calculate emotion distribution
+    emotion_dist = [int(100 * emotions.count(emotion) / len(emotions)) for emotion in SER._emotion.values()]
+    # Export emotion distribution to .csv format for D3JS
+    df = pd.DataFrame(emotion_dist, index=SER._emotion.values(), columns=['VALUE']).rename_axis('EMOTION')
+    df.to_csv(os.path.join('static', 'js','audio_emotions_dist.txt'), sep=',')
+
+    df_other = pd.read_csv(os.path.join("static","js", "audio_emotions_other.txt"), sep=",")
+
+    major_emotion_other = df_other.EMOTION.mode()[0]
+
+ 
+    emotion_dist_other = [int(100 * len(df_other[df_other.EMOTION==emotion]) / len(df_other)) for emotion in SER._emotion.values()]
+
+    # Export emotion distribution to .csv format for D3JS
+    df_other = pd.DataFrame(emotion_dist_other, index=SER._emotion.values(), columns=['VALUE']).rename_axis('EMOTION')
+    df_other.to_csv(os.path.join('static', 'js','audio_emotions_dist_other.txt'), sep=',')
+    
+    
+        #emotion_dist_other[4]=emotion_dist_other[4]-30
+    return render_template('audio_analysis.html', emo=major_emotion, emo_other=major_emotion_other, prob=emotion_dist, prob_other=emotion_dist_other)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
